0942b52fdf513cbbf0853042bedaf08aad3dba2d
Qwinn1 <35168918+Qwinn1@users.noreply.github.com>
2021-09-13 16:11:19 -0400

    Fix forktargets, forkports

--- ftbuildfarmerlist.sh
+++ ftbuildfarmerlist.sh
@@ -7,7 +7,7 @@ FARMERLIST=$(ps -ef | grep _farmer | grep -v grep | awk '{print $8}' | sed 's/_f
 CHIAPORTINUSE=$(ss -atnp 2>/dev/null | grep '"chia_farm' | grep ":8559 " | wc -l | awk '{$1=$1};1')
 if [[ $CHIAPORTINUSE == 0 ]]; then
   FARMERLIST=$(echo $FARMERLIST | sed '/^chia$/d')
-  FARMERCOUNT=$(echo (( $FARMERCOUNT - 1 )) )  
+  FARMERCOUNT=$(echo $(( "$FARMERCOUNT - 1" )) )  
 fi
 
 # Add special handling for obnoxious horribly coded forks that use "chia_farmer" as process name
@@ -15,27 +15,27 @@ fi
 XCHAPORTINUSE=$(ss -atnp 2>/dev/null | grep '"chia_farm' | grep ":5159 " | wc -l | awk '{$1=$1};1')
 if [[ $XCHAPORTINUSE == 1 ]]; then 
   FARMERLIST=$(echo -e $FARMERLIST"\nxcha" )
-  FARMERCOUNT=$(echo (( $FARMERCOUNT + 1 )) )  
+  FARMERCOUNT=$(echo $(( $FARMERCOUNT + 1 )) )  
 fi  
 LUCKYPORTINUSE=$(ss -atnp 2>/dev/null | grep '"chia_farm' | grep ":16659 " | wc -l | awk '{$1=$1};1')
 if [[ $LUCKYPORTINUSE == 1 ]]; then 
   FARMERLIST=$(echo -e $FARMERLIST"\nlucky" )
-  FARMERCOUNT=$(echo (( $FARMERCOUNT + 1 )) )    
+  FARMERCOUNT=$(echo $(( $FARMERCOUNT + 1 )) )    
 fi  
 NCHAINPORTINUSE=$(ss -atnp 2>/dev/null | grep '"chia_farm' | grep ":38559 " | wc -l | awk '{$1=$1};1')
 if [[ $NCHAINPORTINUSE == 1 ]]; then 
   FARMERLIST=$(echo -e $FARMERLIST"\nnchain" )
-  FARMERCOUNT=$(echo (( $FARMERCOUNT + 1 )) )  
+  FARMERCOUNT=$(echo $(( $FARMERCOUNT + 1 )) )  
 fi  
 FISHERYPORTINUSE=$(ss -atnp 2>/dev/null | grep '"chia_farm' | grep ":4799 " | wc -l | awk '{$1=$1};1')
 if [[ $FISHERYPORTINUSE == 1 ]]; then 
   FARMERLIST=$(echo -e $FARMERLIST"\nfishery" )
-  FARMERCOUNT=$(echo (( $FARMERCOUNT + 1 )) )  
+  FARMERCOUNT=$(echo $(( $FARMERCOUNT + 1 )) )  
 fi  
 ROSEPORTINUSE=$(ss -atnp 2>/dev/null | grep '"chia_farm' | grep ":8459 " | wc -l | awk '{$1=$1};1')
 if [[ $ROSEPORTINUSE == 1 ]]; then 
   FARMERLIST=$(echo -e $FARMERLIST"\nrose" )
-  FARMERCOUNT=$(echo (( $FARMERCOUNT + 1 )) )  
+  FARMERCOUNT=$(echo $(( $FARMERCOUNT + 1 )) )  
 fi
 FARMERLIST=$(echo $FARMERLIST | sort)  
 IFS=$OLDIFS
--- ftbuildharvesterlist.sh
+++ ftbuildharvesterlist.sh
@@ -14,27 +14,27 @@ fi
 XCHAPORTINUSE=$(ss -atnp 2>/dev/null | grep '"chia_harv' | grep ":5160 " | wc -l | awk '{$1=$1};1')
 if [[ $XCHAPORTINUSE == 1 ]]; then 
   HARVESTERLIST=$(echo -e $HARVESTERLIST"\nxcha" )
-  HARVESTERCOUNT=$(echo (( $HARVESTERCOUNT + 1 )) )
+  HARVESTERCOUNT=$(echo $(( $HARVESTERCOUNT + 1 )) )
 fi  
 LUCKYPORTINUSE=$(ss -atnp 2>/dev/null | grep '"chia_harv' | grep ":16660 " | wc -l | awk '{$1=$1};1')
 if [[ $LUCKYPORTINUSE == 1 ]]; then 
   HARVESTERLIST=$(echo -e $HARVESTERLIST"\nlucky" )
-  HARVESTERCOUNT=$(echo (( $HARVESTERCOUNT + 1 )) )  
+  HARVESTERCOUNT=$(echo $(( $HARVESTERCOUNT + 1 )) )  
 fi  
 NCHAINPORTINUSE=$(ss -atnp 2>/dev/null | grep '"chia_harv' | grep ":38560 " | wc -l | awk '{$1=$1};1')
 if [[ $NCHAINPORTINUSE == 1 ]]; then 
   HARVESTERLIST=$(echo -e $HARVESTERLIST"\nnchain" )
-  HARVESTERCOUNT=$(echo (( $HARVESTERCOUNT + 1 )) )  
+  HARVESTERCOUNT=$(echo $(( $HARVESTERCOUNT + 1 )) )  
 fi  
 FISHERYPORTINUSE=$(ss -atnp 2>/dev/null | grep '"chia_harv' | grep ":4790 " | wc -l | awk '{$1=$1};1')
 if [[ $FISHERYPORTINUSE == 1 ]]; then 
   HARVESTERLIST=$(echo -e $HARVESTERLIST"\nfishery" )
-  HARVESTERCOUNT=$(echo (( $HARVESTERCOUNT + 1 )) )  
+  HARVESTERCOUNT=$(echo $(( $HARVESTERCOUNT + 1 )) )  
 fi  
 ROSEPORTINUSE=$(ss -atnp 2>/dev/null | grep '"chia_harv' | grep ":8561 " | wc -l | awk '{$1=$1};1')
 if [[ $ROSEPORTINUSE == 1 ]]; then 
   HARVESTERLIST=$(echo -e $HARVESTERLIST"\nrose" )
-  HARVESTERCOUNT=$(echo (( $HARVESTERCOUNT + 1 )) )  
+  HARVESTERCOUNT=$(echo $(( $HARVESTERCOUNT + 1 )) )  
 fi  
 HARVESTERLIST=$(echo $HARVESTERLIST | sort)  
 IFS=$OLDIFS
