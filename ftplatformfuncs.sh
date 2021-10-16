5b76354aff7859cd62779f76e18ba187ed38f7cf
Qwinn1 <pescalona@outlook.com>
2021-10-16 14:34:57 -0400

    Make forkports output platform specific

--- ftplatformfuncs.sh
+++ ftplatformfuncs.sh
@@ -10,6 +10,9 @@ if [[ $OSTYPE == 'darwin'* ]]; then
     function forkss () {
       join <(ps x -o pid,comm | awk '{print $1 " \"" $2 "\""}' | sort) <(lsof -i4 -i TCP -n | grep LISTEN | awk '{print $2 " " $9 " :"}' | sort)
     }
+    function forkssoutput () {
+      join <(ps x -o pid,comm | awk '{print $1 " \"" $2 "\""}' | sort) <(lsof -i4 -i TCP -n | grep LISTEN | awk '{print $2 " " $9 " :"}' | sort)    
+    }
     function forkmemory () {
       ps -x -o rss= -p $(pgrep ^${fork}_) | awk '{ sum +=$1/1024 } END {printf "%7.0f MB\n", sum}'
     }
@@ -25,6 +28,25 @@ else
     }
     function forkss () {
       ss -atnp 2>/dev/null | grep -v TIME-WAIT | grep -v FIN-WAIT
+    }
+    function forkssoutput () {
+      if [[ $FORKNAME == 'littlelambocoin' ]]; then
+        CONFLICTS=$( forkss | grep :$port[^0-9] | grep -v '"'${FORKNAME} | sed "s/      / /g"   | sed 's/((//' | grep -Eo '.*users:"[^"]*["]' )
+      else
+        CONFLICTS=$( forkss | grep :$port[^0-9] | grep -v '"'${FORKNAME}_ | sed "s/      / /g"   | sed 's/((//' | grep -Eo '.*users:"[^"]*["]' )
+      fi
+      if [[ $CONFLICTS != '' ]]; then
+         echo "                          $fork port $port in use by:"
+         OLDIFS=$IFS
+         IFS=''
+         TITLES=$( ss -atnp | sed "s/      / /g" | head -1 )
+         TITLES=$( awk '{ printf ("%-8.8s %-9.9s %-12.12s %-5.5s %-19.19s %-4.4s %-16.16s %-50s\n", $1, $2, $3, $4, $5, $6, $7, $8 ); }' <<< "$TITLES" )
+         echo $TITLES
+         CONFLICTS=$( awk '{ printf ("%-8.8s %-9.9s %-12.12s %-25.25s %-21.21s %-50s\n", $1, $2, $3, $4, $5, $6 ); }' <<< "$CONFLICTS" )
+#         CONFLICTS=$( echo $CONFLICTS | awk '{$1=$1};1' )      
+         echo $CONFLICTS         
+         IFS=$OLDIFS         
+      fi
     }
     function forkmemory () {
       for pid in $(pgrep ^${fork}_); do 
