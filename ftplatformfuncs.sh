3c99912b1ba1792ecb09f743e32f400c5db0db6a
Qwinn1 <pescalona@outlook.com>
2021-10-17 14:23:03 -0400

    Update ftplatformfuncs.sh

--- ftplatformfuncs.sh
+++ ftplatformfuncs.sh
@@ -35,27 +35,26 @@ else
       date -d $2"${1} day" +"%Y-%m-%d"
     }
     function forkss () {
-      ss -atnp 2>/dev/null | grep -v TIME-WAIT | grep -v FIN-WAIT
+      ss -atnp 2>/dev/null | awk '{ printf "%s %s %s\n", $1, $4, $6 }' | grep -v TIME-WAIT | grep -v FIN-WAIT
     }
     function forkssoutput () {
       if [[ $FORKNAME == 'littlelambocoin' ]]; then
-        CONFLICTS=$( forkss | grep :$port[^0-9] | grep -v '"'${FORKNAME} | sed "s/      / /g"   | sed 's/((//' | grep -Eo '.*users:"[^"]*["]' )
+        CONFLICTS=$( forkss | grep :$port[^0-9] | grep -v '"'${FORKNAME} | sed 's/((//' | grep -Eo '.*users:"[^"]*["]' | sed 's/users://' | sed 's/"//g' )
       else
-        CONFLICTS=$( forkss | grep :$port[^0-9] | grep -v '"'${FORKNAME}_ | sed "s/      / /g"   | sed 's/((//' | grep -Eo '.*users:"[^"]*["]' | sed 's/users://' | sed 's/"//g' )
+        CONFLICTS=$( forkss | grep :$port[^0-9] | grep -v '"'${FORKNAME}_ | sed 's/((//' | grep -Eo '.*users:"[^"]*["]' | sed 's/users://' | sed 's/"//g' )
       fi
       if [[ $CONFLICTS != '' ]]; then
          if [[ $SCANNEDMSG == 0 ]]; then
             printf "Scanned %-15.15s - Conflicts Found!\n" $fork
             SCANNEDMSG=1
          fi
-         echo "                          $fork port $port in use by:"
          OLDIFS=$IFS
          IFS=''
-         TITLES=$( ss -atnp | sed "s/      / /g" | head -1 )
-         TITLES=$( awk '{ printf ("%-8.8s %-9.9s %-12.12s %-5.5s %-19.19s %-4.4s %-16.16s %-50s\n", $1, $2, $3, $4, $5, $6, $7, $8 ); }' <<< "$TITLES" )
+         TITLES=$( ss -atnp | head -1 | awk '{$1=$1};1' )
+         TITLES=$( awk    '{ printf ("                                       %-8.8s %-5.5s %-19.19s %-50s\n", $1, $4, $5, $8 ); }' <<< "$TITLES" )
          echo $TITLES
-         CONFLICTS=$( awk '{ printf ("%-8.8s %-9.9s %-12.12s %-25.25s %-21.21s %-50s\n", $1, $2, $3, $4, $5, $6 ); }' <<< "$CONFLICTS" )
-#         CONFLICTS=$( echo $CONFLICTS | awk '{$1=$1};1' )      
+         CONFDESC=$(echo "$fork port $port in use by:" )
+         CONFLICTS=$( awk -v confdesc="$CONFDESC" '{ printf ("%-38.38s %-8.8s %-25.25s %-50s\n", confdesc, $1, $2, $3 ); }' <<< "$CONFLICTS" )
          echo $CONFLICTS         
          IFS=$OLDIFS
       fi
