7b2f22fbbcf2653f7777f6ae675de58df5c102f3
Qwinn1 <pescalona@outlook.com>
2021-10-17 00:18:21 -0400

    Improved output format

--- ftplatformfuncs.sh
+++ ftplatformfuncs.sh
@@ -13,8 +13,13 @@ if [[ $OSTYPE == 'darwin'* ]]; then
     function forkssoutput () {
       CONFLICTS=$( forkss | grep :$port[^0-9] | grep -v '"'${FORKNAME}_ )
       if [[ $CONFLICTS != '' ]]; then
+         if [[ $SCANNEDMSG == 0 ]]; then
+            printf "Scanned %-15.15s - Conflicts Found!\n" $fork
+            SCANNEDMSG=1
+         fi
          echo "$fork port $port in use by:  $CONFLICTS"
       fi
+      return 0
     }
     function forkmemory () {
       ps -x -o rss= -p $(pgrep ^${fork}_) | awk '{ sum +=$1/1024 } END {printf "%7.0f MB\n", sum}'
@@ -39,6 +44,10 @@ else
         CONFLICTS=$( forkss | grep :$port[^0-9] | grep -v '"'${FORKNAME}_ | sed "s/      / /g"   | sed 's/((//' | grep -Eo '.*users:"[^"]*["]' | sed 's/users://' | sed 's/"//g' )
       fi
       if [[ $CONFLICTS != '' ]]; then
+         if [[ $SCANNEDMSG == 0 ]]; then
+            printf "Scanned %-15.15s - Conflicts Found!\n" $fork
+            SCANNEDMSG=1
+         fi
          echo "                          $fork port $port in use by:"
          OLDIFS=$IFS
          IFS=''
@@ -48,7 +57,7 @@ else
          CONFLICTS=$( awk '{ printf ("%-8.8s %-9.9s %-12.12s %-25.25s %-21.21s %-50s\n", $1, $2, $3, $4, $5, $6 ); }' <<< "$CONFLICTS" )
 #         CONFLICTS=$( echo $CONFLICTS | awk '{$1=$1};1' )      
          echo $CONFLICTS         
-         IFS=$OLDIFS         
+         IFS=$OLDIFS
       fi
     }
     function forkmemory () {
