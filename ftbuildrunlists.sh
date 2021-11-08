b2368bc4d5e2cbc84c8d15eb76150ce081f2afd8
Qwinn1 <pescalona@outlook.com>
2021-11-08 00:07:19 -0500

    Stopped forks

--- ftbuildrunlists.sh
+++ ftbuildrunlists.sh
@@ -3,6 +3,7 @@ FORKLIST=$( ls -d1 *-blockchain | sed 's/-blockchain//' )
 FARMERLIST=$FORKLIST
 HARVESTERLIST=$FORKLIST
 DAEMONLIST=$FORKLIST
+STOPPEDLIST=''
 
 PROCESSEF=$( getproclist )
 
@@ -12,7 +13,7 @@ for FORKNAME in $FORKLIST; do
   if [[ ! -f $FORKTOOLSHIDDENDIRS/.$FORKNAME/mainnet/config/config.yaml ]]; then
      FARMERLIST=$(echo $FARMERLIST | grep -v "^$FORKNAME$" )
      HARVESTERLIST=$(echo $HARVESTERLIST | grep -v "^$FORKNAME$" )
-     DAEMONLIST=$(echo $DAEMONLIST | grep -v "^$FORKNAME$" )               
+     DAEMONLIST=$(echo $DAEMONLIST | grep -v "^$FORKNAME$" )
      continue
   else
      CURRENTCONFIG=$FORKTOOLSHIDDENDIRS/.$FORKNAME/mainnet/config/config.yaml
@@ -27,11 +28,18 @@ for FORKNAME in $FORKLIST; do
   if [[ $DAEMONRUNNING != 1 ]]; then
      DAEMONLIST=$(echo $DAEMONLIST | grep -v "^$FORKNAME$" )
   fi
+  if [[ $FARMERRUNNING == 0 && $HARVESTERRUNNING == 0 ]]; then
+     STOPPEDLIST=$(printf '%s\n%s' $STOPPEDLIST $FORKNAME )
+  fi
 done
+
+
+  
 
 IFS=$'\n'
 FARMERCOUNT=$(echo $FARMERLIST | wc -w | awk '{$1=$1};1')
 HARVESTERCOUNT=$(echo $HARVESTERLIST | wc -w | awk '{$1=$1};1')
 DAEMONCOUNT=$(echo $DAEMONLIST | wc -w | awk '{$1=$1};1')
+STOPPEDCOUNT=$(echo $STOPPEDLIST | wc -w | awk '{$1=$1};1')
 IFS=$OLDIFSBRL
 
