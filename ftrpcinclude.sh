e660e426b5b26a76386cd63a0456c3c319a51f6e
Qwinn1 <pescalona@outlook.com>
2021-11-12 16:46:51 -0500

    Update ftrpcinclude.sh

--- ftrpcinclude.sh
+++ ftrpcinclude.sh
@@ -33,10 +33,6 @@ COINNAME=$(echo $COINNAME | sed 's/.*"network_prefix": "//' | sed 's/",.*//' | t
 
 # Get major-minor multiplier
 MMMULTIPLIERNAME=$FORKNAME
-# Hard coding to account for crappy lack of proper fork renaming. 
-if [[ $MMMULTIPLIERNAME == 'rolls' ]]; then
-   MMMULTIPLIERNAME='roll'
-fi
 MMMULTIPLIER=$( cat $FORKTOOLSBLOCKCHAINDIRS/$FORKNAME-blockchain/$FORKNAME/consensus/block_rewards.py | grep "^_.*_per_$MMMULTIPLIERNAME =" | sed 's/.*=//' | sed 's/_//g' | sed 's/\*.*//' | awk '{$1=$1};1')
 if [[ $MMMULTIPLIER == '' ]]; then
    MMMULTIPLIER=$( cat $FORKTOOLSBLOCKCHAINDIRS/$FORKNAME-blockchain/$FORKNAME/consensus/block_rewards.py | grep "^_.*_per_chia =" | sed 's/.*=//' | sed 's/_//g' | sed 's/\*.*//' | awk '{$1=$1};1')
